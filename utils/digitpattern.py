b6cff09e2cada7bb60c5bdd616e45fb7a5360f92
Rails to Cosmos <akatovda@yandex.com>
2016-02-03 18:10:19 +0300

    digit pattern bugfixes

--- utils/digitpattern.py
+++ utils/digitpattern.py
@@ -3,26 +3,28 @@ from datetime import datetime
 
 
 def digit_pattern(content, idate):
-        pattern = r'\d{1,2}\.\d{1,2}\.\d{2,4}'
-        matches = re.findall(pattern, content)
+    patterns = ['(?P<day>\d{1,2})\.(?P<month>\d{1,2})\.(?P<year>\d{4})',
+                '(?P<year>\d{4})\.(?P<month>\d{2})\.(?P<day>\d{2})',
+                '(?P<year>\d{4})\-(?P<month>\d{2})\-(?P<day>\d{2})']
 
-        for match in matches:
-            pattern = r'\d{1,4}'
-            lmatches = re.findall(pattern, match)
+    for pattern in patterns:
+        try:
+            date = re.search(pattern, content).groupdict()
+        except AttributeError:
+            continue
 
-            year = lmatches[2]
+        if len(date['year']) == 2:
+            date['year'] = '20{year}'.format(year=date['year'])
 
-            if len(year) == 2:
-                year = '20{year}'.format(year=year)
-            year = int(year)
+        date = {
+            'day': int(date['day']),
+            'month': int(date['month']),
+            'year': int(date['year']),
+            'hour': idate.hour,
+            'minute': idate.minute,
+            'second': idate.second
+        }
+        idate = datetime(**date)
+        break
 
-            date = {
-                'day': int(lmatches[0]),
-                'month': int(lmatches[1]),
-                'year': year
-            }
-
-        if matches:
-            idate = datetime(**date)
-
-        return content, idate
+    return content, idate
